7f7bee44442510cbc025a05f36fae8fdf49a3a32
Bhekisile <bhekisilejozi@gmail.com>
2023-01-26 09:36:31 +0200

    Add a button to remove the book from the library

--- main.js
+++ main.js
@@ -12,10 +12,18 @@ function addBookToLibrary(title, author, pages, read) {
   // do stuff here
   let book = new Book(title, author, pages, read);
   myLibrary.push(book);
+  displayBook();
 }
 
 function displayBook() {
   const books = document.querySelector('.books');
+
+  const removeDivs = document.querySelectorAll('.card');
+  console.log('show me the node count of current card divs...', removeDivs);
+  for (let i=0; i < removeDivs.length; i++) {
+  removeDivs[i].remove();
+}
+
     myLibrary.forEach(myLibrary => {
       const card = document.createElement('div');
       card.classList.add('card');
@@ -23,28 +31,53 @@ function displayBook() {
       for (let key in myLibrary) {
         console.log(key + ":" + myLibrary[key]);
         const para = document.createElement('p');
-        para.textContent = (key + ":" + myLibrary[key]);
+        para.innerText = (key + ":" + myLibrary[key]);
         card.appendChild(para);
-      }
+    }
     })
+    const button = document.createElement('button');
+    const text = document.createTextNode('Remove book');
+    button.classList = "second btn";
+    button.appendChild(text);
+    books.appendChild(button);
+    button.onclick = removeBook(Book);
+    /*addEventListener('click', event => {
+      event.target.parentNode.remove();
+      myLibrary = myLibrary.filter(v => v[1] !== event.target);
+    })*/
 }
+
+function removeBook(Book) {
+  console.log('Library', myLibrary);
+  myLibrary.pop(Book);
+  }
 
 function showForm() {
   document.getElementById('newBook').style.display = 'block';
 }
 
-/*function intakeFormData() {
-  let title = document.getElementsByName("title").value;
-  let author = document.getElementsByName("author").value;
-  let pages = document.getElementsByName("pages").value;
-  let read = document.getElementsByName("read").value;
+function intakeFormData() {
+  let title = document.querySelector(".title").value;
+  let author = document.querySelector(".author").value;
+  let pages = document.querySelector(".pages").value;
+  let read = document.querySelector(".read").value;
 
   if ((title === "") || (author === "") || (pages === "") || (read === "")){
     return;
-  }*/
+  }
+  addBookToLibrary(title, author, pages, read);
+  document.getElementById("newBook").reset();
+}
 
-addBookToLibrary("The Hobbit", " J.R.R. Tolkien", 250, "Yes");
+/*addBookToLibrary("The Hobbit", " J.R.R. Tolkien", 250, "Yes");
 addBookToLibrary("Animal Farm", " J.R.R. Tolkien", 500, "Yes");
 addBookToLibrary("Beauty and the Beast", " J.R.R. Tolkien", 700, "Not Yet");
 addBookToLibrary("The Hostage", " J.R.R. Tolkien", 400, "Not yet");
-displayBook();
+displayBook();*/
+
+const signupForm = document.querySelector('#newBook');
+signupForm.addEventListener("submit",
+function(event) {
+  console.log(event);
+  event.preventDefault();
+});
