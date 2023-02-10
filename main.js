0d0a2ba85c60ac600a15a1ea4f3d0aa9b53504a5
Bhekisile <bhekisilejozi@gmail.com>
2023-02-10 16:38:00 +0200

    edit main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.status = status
+  this.status = status;
 }
 
 function addBookToLibrary(title, author, pages, status) {
